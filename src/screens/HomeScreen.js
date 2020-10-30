48b5a1f26ec789d5268a2f88f4960440a16e6fed
“[Nicol <[nicol.saha@gmail.com]>
2020-10-30 12:42:14 +0100

    Add selected btns step one screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -71,7 +71,7 @@ const HomeScreen = () => {
                         </div>
 
                         <div className="mt-8 ml-32">
-                            <button onClick={handleBtnStepOne} className="inline-flex rounded-3xl shadow">
+                            <button className="inline-flex rounded-3xl shadow">
                                 <Link to={'/step1'}
                                     className="font-body inline-flex items-center justify-center bg-mainLavender hover:bg-subLavender text-white hover:font-semibold px-8 py-3 rounded-3xl focus:outline-none focus:shadow-outline transition duration-100 ease-in-out">
                                         GET STARTED 	&rarr;
